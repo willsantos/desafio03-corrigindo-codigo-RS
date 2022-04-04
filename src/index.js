3caa9c5cefe1c04e0993f299d9e3823609d5f417
Wilson Santos <contato@wilsonsantos.net>
2022-04-04 15:22:33 -0300

    fix: fix incorrect logic on findIndex validation

--- src/index.js
+++ src/index.js
@@ -42,16 +42,16 @@ app.put("/repositories/:id", (request, response) => {
   repository.url = url;
   repository.techs = techs;
 
-
+  
   return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
